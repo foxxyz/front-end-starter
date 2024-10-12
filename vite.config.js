991aa1ef77f5036ef7959a21f25127d4e49048d4
foxxyz <foxxyz@gmail.com>
2024-10-12 16:56:50 -0700

    build: upgrade deps to latest; use vite6 beta

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 export default {
+    css: {
+        preprocessorOptions: {
+            sass: {
+                api: 'modern-compiler'
+            }
+        }
+    },
     define: {
         __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
     },
